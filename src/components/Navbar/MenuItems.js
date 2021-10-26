fe540a0a64767a958454f2b180e8d2a92c2a533e
Matthew Ty <matthewty1220@gmail.com>
2021-10-26 16:19:52 -0600

    initial commit

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,28 @@
+
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Plans',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Entries',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Profile',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign Up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+]
